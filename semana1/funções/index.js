8d703a2e3ae390166a3631e06e293f40ea9a5b24
Luis Claudio Boaventura <Luizinboaventura@gmail.com>
2021-05-06 15:08:57 -0300

    Exercicios de Função

---
+++ semana1/funções/index.js
@@ -0,0 +1,63 @@
+// INTERPRETAÇÃO DE CÓDIGO
+
+//<--------------------- Exercicio 1 --------------------
+
+/*
+
+1 - Leia o código abaixo
+
+function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+(minhaFuncao(2))
+(minhaFuncao(10))
+
+a) O que vai ser impresso no console?
+ 
+    Vai ser impresso no console 10 e 50 que é o return após o a execução da função 
+
+b) O que aconteceria se retirasse os dois console.log e simplesmente invocasse a função minhaFuncao(2) e minhaFuncao(10)? O que apareceria no console?
+
+    Aconteceria um erro porque a função está dentro de um escopo fechado o que não permite que ela seja reconhecida. Apareceria minhaFuncao()..is not a function 
+
+*/
+
+//<--------------------- Exercicio 2 --------------------
+
+/*
+
+2 - Leia o código abaixo
+
+let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)
+
+a) Explique o que essa função faz e qual é sua utilidade
+
+     A função deixa texto de entrada todo em letras minusculas e verfifica se possui a palavra cenoura presente no texto, se tiver retorna True no console, se não tiver retona False
+
+b)  Determine qual será a saída no console para cada uma das 3 entradas do usuário:
+
+    1)Eu gosto de cenoura -> retornara True no console
+    2)CENOURA é bom pra vista -> retornara True no console 
+    3)Cenouras crescem na terra -> retonara 
+
+*/
+
+//ESCRITA DE CÓDIGO
+
+//<--------------------- Exercicio 1  --------------------
+
+
+
+
+//<--------------------- Exercicio 2 --------------------
+
+
+//<--------------------- Exercicio 3 --------------------
