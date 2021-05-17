82aa3bb8d13e9c7a17001ff31489d3ab64bb7259
Luis Claudio Boaventura <Luizinboaventura@gmail.com>
2021-05-17 16:17:00 -0300

    Exercicio 8 concluido

--- semana3/revisao-js/lista-js-boilerplate/exercicios.js
+++ semana3/revisao-js/lista-js-boilerplate/exercicios.js
@@ -56,7 +56,13 @@ function retornaNNumerosPares(n) {
 // Exercício 8
 
 function checaTriangulo(a, b, c) {
-  // implemente sua lógica aqui
+   if(a !== b && b !== c) {
+      return "Escaleno"
+     } else if(a === b && b === c) {
+       return "Equilátero"
+      } else {
+        return "Isósceles"
+      }
 }
 
 // Exercício 9
