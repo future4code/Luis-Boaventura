b710dfcabac985f866f1b96f830d1137cce6f7c7
Luis Claudio Boaventura <Luizinboaventura@gmail.com>
2021-06-15 18:22:23 -0300

    pagina para adicao de usuarios

---
+++ semana7/Integracao-API/lista-usuarios/src/components/global.js
@@ -0,0 +1,19 @@
+//Componente criado para zerar as configurações de css e
+// determinar algumas configurações globais.
+
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box
+  }
+  body, input, button {
+    font: 14px sans-serif;
+  }
+  button {
+    cursor: pointer;
+  }
+`;
