b1d90d15efda6787fcaf3c99c9439a8d65bb036b
Luis Claudio Boaventura <Luizinboaventura@gmail.com>
2021-08-09 15:48:16 -0300

    Aula de node com exercicios, ainda imcompleto

---
+++ semana15/Node_e_PackageJson/template-node-npm/src/Exercicio1b.js
@@ -0,0 +1,6 @@
+const Nomequery = process.argv[2]
+const Numeroquery = Number(process.argv[3])
+
+const resultado = `Olá ${Nomequery}! você tem ${Numeroquery} anos.`
+
+console.log (resultado)
---
+++ semana15/Node_e_PackageJson/template-node-npm/src/index.js
@@ -0,0 +1,29 @@
+/*
+Exercicios 1 
+
+a)  Responda como comentário no seu código: como fazemos para acessar os parâmetros passados na linha de comando para o Node?
+
+Resposta : Para acessar os parametros precisamos usar a propriedade porcess.argv com indice [2] ou seja process.argv[2] que lê os parametros. Na chamada 
+
+b) Crie um programa que receba seu nome e sua idade. Após receber estes valores, imprima no console uma mensagem que siga a seguinte estrutura:
+
+Resposta : Exercicio feito, rodar arquivo "apareceNome.js" no terminal através do comando "npm run nome_no_console Luis 29"
+
+c)Altere o programa acima para que mostre também a sua idade daqui a sete anos.
+
+Resposta : Exercicio feito, rodar arquivo "idadeSeteAnos.js" no terminal através do comando "npm run idade Luis 29"
+
+*/
+
+
+
+
+
+
+
+
+/*
+const countries = require("./countries")
+console.log(countries)
+
+*/
